9b4250be4a639862c15ed1165a7304d2c2317fca
Soveet Nayak <soveet.nayak@gmail.com>
2023-10-25 19:32:09 +0530

    homepage dump

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Login from './components/Login.js';
 import HomePage from './components/Home.js';
+import Layout from './components/Home-react.js';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
        <BrowserRouter>
        <Routes>
           <Route path="/" element={<Login/>}/>
-        <Route path="/home" element={<HomePage/>}/>
+        {/* <Route path="/home" element={<HomePage/>}/> */}
+          <Route path="/home" element={<Layout/>}/>
         </Routes>
       </BrowserRouter> 
     </div>
